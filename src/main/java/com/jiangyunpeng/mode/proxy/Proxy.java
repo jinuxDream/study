a793456228cd697ea48933c3a1ca865698d08641
JinuxDream <422053690@qq.com>
2017-10-23 21:48:07 +0800

    adept

---
+++ src/main/java/com/jiangyunpeng/mode/proxy/Proxy.java
@@ -0,0 +1,7 @@
+package com.jiangyunpeng.mode.proxy;
+
+/**
+ * Created by jyp on 2017/10/18.
+ */
+public class Proxy {
+}
---
+++ src/main/java/com/jiangyunpeng/mode/proxy/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 代理模式
+ * Created by jyp on 2017/10/18.
+ */
+package com.jiangyunpeng.mode.proxy;
